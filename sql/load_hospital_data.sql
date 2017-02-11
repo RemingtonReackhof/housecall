3c0c6521325a4c497e3a44c71c97b9acfb2007d8
Matt Fox <mattfox@umich.edu>
2017-02-11 15:41:32 -0500

    SQL code to create tables and load hospital data

---
+++ sql/load_hospital_data.sql
@@ -0,0 +1,77 @@
+INSERT INTO Hospital
+VALUES ('1', "University Hospital");
+
+INSERT INTO Hospital
+VALUES ('2', "C.S. Mott's Children's Hospital");
+
+INSERT INTO Hospital
+VALUES ('3', "Magnetic Resonance Imaging (MRI) at University Hospital");
+
+INSERT INTO Hospital
+VALUES ('4', "Neurology at Mott's Children's Hospital");
+
+INSERT INTO Hospital
+VALUES ('5', "Nephrology Clinic at Mott's Children's Hospital");
+
+INSERT INTO Hospital
+VALUES ('6', "Medical Procedures Unit at University Hospital");
+
+INSERT INTO Hospital
+VALUES ('7', "Med Inn Building");
+
+INSERT INTO Hospital
+VALUES ('8', "Pulmonary Clinic at Mott's Children's Hospital");
+
+INSERT INTO Hospital
+VALUES ('9', "Neurology Clinic at Taubman Center");
+
+INSERT INTO Hospital
+VALUES ('10', "Ultrasound at University Hospital");
+
+INSERT INTO Hospital
+VALUES ('11', "Gastroenterology at Mott's Children's Hospital");
+
+INSERT INTO Hospital
+VALUES ('12', "Univesity Hospital Psychiatric Emergency Service");
+
+INSERT INTO Hospital
+VALUES ('13', "U of M Hospital");
+
+INSERT INTO Hospital
+VALUES ('14', "Department of Radiation Oncology");
+
+INSERT INTO Hospital
+VALUES ('15', "Von Voigtlander Women's Hospital");
+
+INSERT INTO Hospital
+VALUES ('16', "Urology Clinic at Taubman Center");
+
+INSERT INTO Hospital
+VALUES ('17', "Emergency Room at University Hospital");
+
+INSERT INTO Hospital
+VALUES ('18', "Multidisciplinary Developmental Evaluation Clinic (M-DEC)");
+
+INSERT INTO Hospital
+VALUES ('19', "Department of Pediatrics and Communicable Diseases");
+
+INSERT INTO Hospital
+VALUES ('20', "Apheresis Procedure Unit at University Hospital");
+
+INSERT INTO Hospital
+VALUES ('21', "VA Ann Arbor Healthcare System");
+
+INSERT INTO Hospital
+VALUES ('22', "Henry Ford Health System");
+
+INSERT INTO Hospital
+VALUES ('23', "St. Joseph Mercy Ann Arbor");
+
+INSERT INTO Hospital
+VALUES ('24', "Select Specialty Hospital - Ann Arbor");
+
+INSERT INTO Hospital
+VALUES ('25', "Michigan Heart & Vascular Institute");
+
+INSERT INTO Hospital
+VALUES ('26', "South Main Orthopaedics");
