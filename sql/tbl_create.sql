089e005ec1e9216cc1875788277170f115fe3f59
Kayla Fedewa <kafedewa@umich.edu>
2017-02-19 22:37:44 -0500

    UI fixes

--- sql/tbl_create.sql
+++ sql/tbl_create.sql
@@ -19,4 +19,15 @@ CREATE TABLE User (
     	ON DELETE CASCADE
 );
 
+CREATE TABLE Notes (
+    note_id INT AUTO_INCREMENT,
+    is_note BOOLEAN,
+    is_instruction BOOLEAN,
+    is_snapshot BOOLEAN,
+    title VARCHAR(100),
+    time_stamp VARCHAR(100),
+    content TEXT,
+    PRIMARY KEY (note_id)
+);
+
 
