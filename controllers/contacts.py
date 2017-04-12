1a359ca41977ac1187e3b20ca83a82a45ad9cb25
Matt Fox <mattfox@umich.edu>
2017-04-12 19:30:44 -0400

    Contacts Page backend

--- controllers/contacts.py
+++ controllers/contacts.py
@@ -9,9 +9,10 @@ import ast
 contacts = Blueprint('contacts', __name__, template_folder='templates') #, url_prefix="/04d8ee3a8730446aa2b4/pa3")
 
 
-@contacts.route('/contacts', methods=['GET'])
+@contacts.route('/contacts', methods=['GET', 'POST'])
 def my_route():
 	#print 'GETTING'
+
 
 	# if not logged in
 	username = request.args.get("username")
@@ -19,5 +20,31 @@ def my_route():
 		if "username" not in session:
 			return redirect(url_for("index.index_route"))
 		username = session['username']
+
+
+
+	if request.method == 'GET':
+
+		cur = mysql.connection.cursor()
+		cur.execute("SELECT user_id, skype_username, firstname, lastname, specialty FROM User WHERE Doctor = 1;")
+		rows = cur.fetchall()
+		print rows
+		if rows is None:
+			return jsonify(successful=False)
+		else:
+			return jsonify(successful=True, rows=rows)
+
+
+	if request.method == 'POST':
+
+		data = ast.literal_eval(request.data)
+
+		cur = mysql.connection.cursor()
+		cur.execute("INSERT INTO Call (emt_id, dr_id, time_stamp, call_length) VALUES (%s, %s, %s, %s)", [ data['emt_id'], data['dr_id'], data['time_stamp'], data['call_length'] ])
+		mysql.connection.commit()
+
+		return render_template("index.html", name='notes')
+
+
 
 	return render_template("index.html", name='contacts')
