cd9f7a3b4ae4edf0360d038d99dc0e07caf2d4a5
Matt Fox <mattfox@umich.edu>
2017-04-17 18:36:28 -0400

    Contacts backend, notes cleanup

--- controllers/contacts.py
+++ controllers/contacts.py
@@ -4,7 +4,7 @@ from flask import Flask, session, redirect, url_for, escape, request, flash
 import hashlib
 import uuid
 import ast
-import datetime
+from datetime import datetime
 
 
 contacts = Blueprint('contacts', __name__, template_folder='templates') #, url_prefix="/04d8ee3a8730446aa2b4/pa3")
@@ -46,29 +46,28 @@ def my_route_2():
 
 	if request.method == 'POST':
 
-		data = ast.literal_eval(request.data)  # emtUsername and dr_id
+		emtEmail = request.form['emtEmail']
+		doctorID = request.form['doctorID']
 
 		# Get EMT's ID using their username
-		emtUsername = data[0]
 		cur = mysql.connection.cursor()
-		cur.execute("SELECT user_id FROM User WHERE skype_username = %s AND Doctor = 0;", emtUsername)
-		emt_id = cur.fetchone()
+		cur.execute("SELECT user_id FROM User WHERE email = (%s)", [emtEmail])
+		emtID = cur.fetchone()[0]
 
 		# Get timestamp
-		timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 		# Insert new row for this call into Call table
 		cur = mysql.connection.cursor()
-		cur.execute("INSERT INTO Call (emt_id, dr_id, time_stamp, call_length) VALUES (%s, %s, %s, %s)", [ emt_id, data[1], timestamp, str(0) ])
+		cur.execute("INSERT INTO `Call`(`emt_id`, `dr_id`, `time_stamp`, `call_length`, `is_active`) VALUES (%s, %s, %s, %s, 1)", [ str(emtID), str(doctorID), str(timestamp), '0:00'])
 		mysql.connection.commit()
 
 		# Get this call's call_id
 		cur = mysql.connection.cursor()
-		cur.execute("SELECT call_id FROM Call ORDER BY call_id DESC LIMIT 1;")
-		callID = cur.fetchone()
+		cur.execute("SELECT `call_id` FROM `Call` ORDER BY `call_id` DESC LIMIT 1")
+		callID = cur.fetchall()[0][0]
 
 		# Return JSON with call id to Garett
 		return jsonify(successful=True, callID=callID)
-
 		#return render_template("index.html", name='notes')
 
--- controllers/notes.py
+++ controllers/notes.py
@@ -14,10 +14,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 UPLOAD_FOLDER = "static/images/"
 
 def allowed_file(filename):
-    #return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    print "shiz"
     if '.' in filename:
-    	print "nat"
     	if filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
     		return True
     return False
@@ -74,6 +71,7 @@ def my_route():
 
 	if request.method == 'POST':	
 
+
 		# If data is a Note or Instruction
 		if not request.files:
 
@@ -91,19 +89,24 @@ def my_route():
 		
 			return jsonify(note_id=recent_note_id) 
 
+
 		# If data is an Image
 		else:
+
 			# check if the post request has the file part
 			if 'file' not in request.files:
 				flash('No file part')
 				return redirect(request.url)
 			file = request.files['file']
+
 			# if user does not select file, browser also
 			# submit a empty part without filename
 			if file.filename == '':
 				flash('No selected file')
 				return redirect(request.url)
+
 			if file and allowed_file(file.filename):
+				
 				filename = secure_filename(file.filename)
 				name_of_file = UPLOAD_FOLDER + filename
 				file.save(name_of_file)
